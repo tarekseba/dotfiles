5372ef3d989daf5517a2501bfb482cba400cea9b
Tarek.Seba <tarek.seba@outlook.fr>
2024-04-21 17:31:59 +0200

    added custom lua function to open definition in vsplit

--- nvim-kicks-v10/lua/keybinds.lua
+++ nvim-kicks-v10/lua/keybinds.lua
@@ -13,11 +13,9 @@ vim.keymap.set('n', '<leader>bs', ':BufferMoveStart<CR>')
 -- UndoTree
 vim.keymap.set('n', '<leader>ut', ':UndotreeToggle<CR>')
 
--- GENERAL
---
--- quickfix
-vim.keymap.set('n', ']q', ':cnext<CR>')
-vim.keymap.set('n', '[q', ':cprev<CR>')
+-- ////////////////////////////// LSP ////////////////////////////////
+-- Go to defintion vsplit
+vim.keymap.set('n', 'gsd', ':LspDefinition<CR>')
 
 -- inlay_hints
 vim.keymap.set('n', '<leader>tih', function()
@@ -29,7 +27,31 @@ vim.keymap.set('n', '<leader>lf', function()
   vim.lsp.buf.format { async = true }
 end)
 
+-- ////////////////////////////// GENERAL ////////////////////////////////
+--
+-- quickfix
+vim.keymap.set('n', ']q', ':cnext<CR>')
+vim.keymap.set('n', '[q', ':cprev<CR>')
+
 -- Move selection
 -- echo getpos("'<") to print position
 vim.keymap.set('v', 'K', ":move '<-2<CR>gv-gv")
 vim.keymap.set('v', 'J', ":move '>+1<CR>gv-gv")
+
+-- ////////////////////////////////// CUSTOM FUNCTIONS AND COMMANDS ////////////////////////////////////////
+function open_def_in_vsplit()
+  vim.lsp.buf_request(0, 'textDocument/definition', vim.lsp.util.make_position_params(), function(err, result)
+    if err ~= nil then
+      print(err)
+    else
+      res = result[1]
+      if res ~= nil then
+        local file = result[1].targetUri
+        local coord = result[1].targetSelectionRange.start
+        vim.cmd(string.format('vsplit %s | call cursor(%s, %s)', file, coord.line + 1, coord.character + 1))
+      end
+    end
+  end)
+end
+
+vim.cmd [[ command! LspDefinition lua open_def_in_vsplit()]]
